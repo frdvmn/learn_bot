fcabae7216d1bc772648d43fa2d1e5db0242fb51
Kolya Kalashnikov <frdvmn@mail.ru>
2024-11-27 13:23:16 +0300

    add random number game function

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import logging
+from random import randint
 from telegram.ext import Application, MessageHandler, CommandHandler, filters
 
 import settings
@@ -15,15 +16,23 @@ def main():
     async def greet_user(update, context):
         await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Добро пожаловать.")
     
+    def play_random_numbers(user_number):
+        bot_number = randint(user_number - 10, user_number + 10)
+        if user_number > bot_number:
+            message = f"Ваше число {user_number}, мое {bot_number}, вы выиграли" 
+        elif user_number == bot_number:
+            message = f"Ваше число {user_number}, мое {bot_number}, ничья"
+        else:
+            message = f"Ваше число {user_number}, мое {bot_number}, вы проиграли"
+        return message
+
     async def guess_number(update, context):
         print(context.args)
         if context.args:
             try:
                 user_number = int(context.args[0])
-                message = f"Ваше число {user_number}"
-            except ValueError:
-                message = "Введите целое число"
-            except TypeError:
+                message = play_random_numbers(user_number)
+            except (ValueError, TypeError):
                 message = "Введите целое число"
         else:
             message = "Введите число"
